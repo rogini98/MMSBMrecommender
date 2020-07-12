eeb3c099de332aa0242e2c2cdde84a46486fe0de
Rogini Runghen <rogini.runghen@gmail.com>
2020-07-12 23:41:31 +1200

    Updated MMSBMrecommender.py
    Updated original MMSBM code to identify visitor and place group memberships

--- Code/mmsbm_recommender.py
+++ Code/mmsbm_recommender.py
@@ -1,260 +1,226 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Mixed membership stochastic block model (Godoy-Lorite et al. 2016) 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#-----------------------------------------------------------------------
+# Following modifications has been done to the original code
+# 1) Script has been broken down into functions, can now be uploaded as module
+# 2) Order of vectors not the same as in the original code
+# 3) Names of variables have been changed
+# 4) theta.dat; eta.dat; proba-K-L.dat are saved for each sampling
+#-----------------------------------------------------------------------
 
+#importing the different modules
 import sys
-import _numpypy as np
-#import numpy as np
+import numpy as np 
 from math import *
 import copy
 import random
-from math import sqrt,exp
+import csv
 
-##############################################################################
+def read_files(training, test, zeros_as_null):
+
+    user_dict = {}
+    place_dict = {}
+    visits = {}
+    linksr = []
+
+    file = open(training,"r")
+    for line in file:
+        about = line.strip().split("\t")
+        if int(about[2])!=0 or not zeros_as_null:
+            try:
+                x = user_dict[about[0]][0]
+                user_dict[about[0]][1] += 1
+            except KeyError:
+                x = len(user_dict)
+                user_dict[about[0]] = [x, 1]
+
+            try:
+                y = place_dict[about[1]][0]
+                place_dict[about[1]][1] += 1
+            except KeyError:
+                y = len(place_dict)
+                place_dict[about[1]] = [y, 1]
+
+            try:
+                v = visits[about[2]]
+            except KeyError:
+                v = len(visits)
+                visits[about[2]] = v
+
+            linksr.append([x, y, v])
+    file.close()
+
+    file = open(test,"r")
+    test=[]
+    for line in file:
+        about = line.strip().split("\t")
+        if int(about[2])!=0 or not zeros_as_null:
+            test.append([about[0], about[1], about[2]])
+    file.close()
+
+    # TODO: run tests for data
+
+    return test, user_dict, place_dict, linksr, visits
+
+def sampling(c, ofolder, linksr, nsampling, iterations, K, L, R,
+             user_dict, n_users, users_denom, place_dict, n_places, places_denom, rat, verbose):
+
+    if verbose:
+        sys.stderr.write(" ".join(['sampling',str(c),'\n']))
+
+    #theta = vector containing the different groups to which each user belongs to
+    theta = np.random.rand(n_users,K) / users_denom[:,np.newaxis]
+
+    #eta = vector containing the different groups to which each place belongs to 
+    eta = np.random.rand(n_places,L) / places_denom[:,np.newaxis]
+
+    # 3d matrix containing random probabilities of ratings across user-group and place-group combos
+    # NOTE: I have changed the structure of this and related variables!!!
+    pr = np.random.rand(R, K, L)
+
+    # normalize the probabilities across ratings
+    # should divide by: sum of all ratings corresponding to a group-group combo
+    pr = pr / pr.sum(axis=0)
+
+    # create empty containers for the calculations that are made during each iteration
+    ntheta = np.zeros((n_users, K))
+    neta = np.zeros((n_places, L))
+    npr = np.zeros((R, K, L))
+
+    #########################################################################################
+    for g in range(iterations):
+        if verbose:
+            sys.stderr.write(" ".join(['iteration',str(g),'\n']))
+
+        # update the parameters using each observed 'rating'
+        for n, m, ra in linksr:
+
+            # calculate the sum of all mixtures for rating ra by
+            # multiplying rating probabilities rowwise by the user group
+            # membership probabilities and columnwise by the place group
+            # membership probabilities
+
+            D = (pr[ra].T * theta[n]).T * eta[m]
+
+            # normalize these values
+            a = D / D.sum()
+
+            # update the new (n) parameter estimates
+            npr[ra] = npr[ra] + a
+            ntheta[n] = ntheta[n] + a.sum(axis=1)
+            neta[m] = neta[m] + a.sum(axis=0)
+
+        # normalize the users' membership probabilities across groups
+        ntheta = ntheta / users_denom[:,np.newaxis]
+
+        # normalize the places' membership probabilities across groups
+        neta = neta / places_denom[:,np.newaxis]
+
+        # normalize the probabilities across ratings            
+        npr = npr / npr.sum(axis=0)
+
+        # create copies of previous values and zero'd estimates as placeholders                        
+        theta = copy.deepcopy(ntheta)
+        eta = copy.deepcopy(neta)
+        pr = copy.deepcopy(npr)
+
+        # restart arrays
+        ntheta = ntheta*0
+        neta = neta*0
+        npr = npr*0
+
+    # calculate the likelihood given the probabilities        
+    Like = 0.
+    for n, m, ra in linksr:
+        D = 0.
+        for l in range(L):
+            for k in range(K):
+                D = D+theta[n][k]*eta[m][l]*pr[ra][k][l]
+        for l in range(L):
+            for k in range(K):
+                Like = Like+(theta[n][k]*eta[m][l]*pr[ra][k][l])*log(D)/D
+
+    if verbose:
+        print Like
+
+    fout = open(ofolder+str(c)+'_Likelihood'+str(K)+'gp'+str(L)+'gp.dat',"w")
+    fout.write('%s\n' % Like)
+    fout.close()
+
+    #theta : gp membership vector of visitors
+    theta_vector = open(ofolder+str(c)+'_visitor_gp'+str(K)+'.dat',"w")
+    for i in user_dict.keys():
+        row_num = user_dict[i][0]
+        for k in range(K):
+            theta_vector.write('%s ' % i)
+            theta_vector.write('%s ' % theta[row_num][k])
+            theta_vector.write('\n')
+    theta_vector.close()
+
+    #eta : gp membership vectors of places
+    eta_vector = open(ofolder+str(c)+'_place_gp'+str(L) +'.dat',"w")
+    for j in place_dict.keys():
+        row_num = place_dict[j][0]
+        for l in range(L):
+            eta_vector.write('%s ' % j)
+            eta_vector.write('%s ' % eta[row_num][l])
+            eta_vector.write('\n')
+    eta_vector.close()
+
+    #pr : probability matrix of observed links between gp of users & gp of places
+    proba_visit = open(ofolder+str(c)+'proba'+str(K)+'gp-'+str(L)+'gp.dat',"w")
+    for k in range(K):
+        for l in range(L):
+            for r in range(R):
+                proba_visit.write('%s ' % pr[r][k][l])
+            proba_visit.write('\n')
+    proba_visit.close()
+
+    #save the probability distribution for c of the links in rat
+    ##i.e. save the probability dist when the rating is 0 or 1.
+
+    """nl = 0
+
+    #This is the test from older versions of the code, but here is not being used
+    for about in test:
+        for r in range(R):
+            pra = 0.
+            for k in range(K):
+                for l in range(L):
+                    pra = pra + theta[user_dict[about[0]]][k]*eta[place_dict[about[1]]][l]*pr[r][k][l]
+            rat[nl][r] = rat[nl][r] + pra/nsampling
+        nl=nl+1"""
 
 
-original=sys.argv[1]	#training
-fh=open(original,"r")
-igot = fh.readlines()
+def run_sampling(training,
+             test,
+             ofolder="../../../data-test/processed/",
+             K=4,
+             L=4,
+             nsampling=500,
+             iterations=200,
+             zeros_as_null=True,
+             verbose=True):
 
-original2=sys.argv[2]	#test
-fh2=open(original2,"r")
-igot2=fh2.readlines()
+    if not zeros_as_null:
+        sys.stderr.write("\nCareful! Zeros in your data will represent a type of interaction and will be used in the sampling.\n\n")
 
+    test, user_dict, place_dict, linksr, visits = read_files(training, test, zeros_as_null)
 
-K=int(sys.argv[3])	#number of groups (mixed-membership) users
-L=int(sys.argv[4]) 	#number of groups (mixed-membership) items
+    n_users = len(user_dict)
+    n_places = len(place_dict)
+    R = len(visits)
 
-try:
-	sampling=int(sys.argv[5])
-except IndexError:
-	sampling=1
-	pass
-try:
-	iterations=int(sys.argv[6])
-except IndexError:
-	iterations=200
-	pass
-linksr={}
-d0={}
-d1={}
-p=0
-m=0
-ratings=[]
-for line in igot:
-	about = line.strip().split('	')
-	linksr[(int(about[0]),int(about[1]))]=int(about[2])
-	try:
-		d0[int(about[0])].append(int(about[1]))
-	except KeyError:
-		d0[int(about[0])]=[int(about[1])]
-	try:
-		d1[int(about[1])].append(int(about[0]))
-	except KeyError:
-		d1[int(about[1])]=[int(about[0])]
-	p=max(int(about[0]),p)
-	m=max(int(about[1]),m)
-	ratings.append(int(about[2]))
-ratings=list(set(ratings))
-ratings.sort()
-R=len(ratings)
-print 'users items ratings R',p ,m, ratings, R
-p=p+1
-m=m+1
+    #TODO: figure if I need to clean this or get random sample
+    rat = np.zeros((len(test),R))
 
-#just in case:
-for i in range(p):
-	try:
-		a=d0[i][0]
-	except KeyError:
-		d0[i]=[]
-for j in range(m):
-	try:
-		a=d1[j][0]
-	except KeyError:
-		d1[j]=[]
+    users_denom = np.asarray(user_dict.values())
+    users_denom = users_denom[users_denom[:,0].argsort(),1]
+    places_denom = np.asarray(place_dict.values())
+    places_denom = places_denom[places_denom[:,0].argsort(),1]
 
-print 'total links', len(linksr.keys())
-############################################
-#save the probability distribution of each link in the sampling:
-rat=[]
-for k in range(len(igot2)):
-	rat.append([])
-	for r in range(R):
-		rat[k].append(0.)
-
-for s in range(sampling):
-	theta=[]
-	ntheta=[]
-	for i in range(p):
-		a=[random.random() for _ in xrange(K)]
-		theta.append(a)
-		ntheta.append([0.]*K)
-	eta=[]
-	neta=[]
-	for i in range(m):
-		a=[random.random() for _ in xrange(L)]
-		eta.append(a)
-		neta.append([0.]*L)
-	pr=[]
-	npr=[]
-	for i in range(K):
-		b=[]
-		c=[]
-		for j in range(L):
-			a=[random.random() for _ in xrange(R)]
-			b.append(a)
-			c.append([0.]*R)
-		pr.append(b)
-		npr.append(c)
-
-	
-	#Normalizations:
-	for i in range(p):
-		for k in range(K):
-			try:
-				theta[i][k]=theta[i][k]/(len(d0[i]))
-			except ZeroDivisionError:
-				pass
-
-	for j in range(m):
-		for l in range(L):
-			try:
-				eta[j][l]=eta[j][l]/(len(d1[j]))
-			except ZeroDivisionError:
-				pass
-	for l in range(L):
-		for k in range(K):
-			D=0.
-			for r in range(R):
-				D=D+pr[k][l][r]
-			for r in range(R):
-				pr[k][l][r]=pr[k][l][r]/D
-	
-#########################################################################################
-	for g in range(iterations):
-		for n in linksr.keys():
-			ra=ratings.index(linksr[n])
-			D=0.	
-			for l in range(L):
-				for k in range(K):
-					D=D+theta[n[0]][k]*eta[n[1]][l]*pr[k][l][ra]
-			for l in range(L):
-				for k in range(K):
-					a=(theta[n[0]][k]*eta[n[1]][l]*pr[k][l][ra])/D
-					ntheta[n[0]][k]=ntheta[n[0]][k]+a
-					neta[n[1]][l]=neta[n[1]][l]+a
-					npr[k][l][ra]=npr[k][l][ra]+a	
-		#Normalizations:
-		err=0.
-		for i in range(p):
-			for k in range(K):
-				try:
-					ntheta[i][k]=ntheta[i][k]/(len(d0[i]))
-				except ZeroDivisionError:
-					pass
-		for j in range(m):
-			for l in range(L):
-				try:
-					neta[j][l]=neta[j][l]/(len(d1[j]))
-				except ZeroDivisionError:
-					pass
-			
-		for l in range(L):
-			for k in range(K):
-				D=0.
-				for r in range(R):
-					D=D+npr[k][l][r]
-				for r in range(R):
-					npr[k][l][r]=npr[k][l][r]/D
-
-		theta=copy.copy(ntheta)
-		eta=copy.copy(neta)	
-		for k in range(K):
-			for l in range(L):
-				pr[k][l]=npr[k][l]
-		for i in range(p):
-			ntheta[i]=[0.]*K
-		for j in range(m):
-			neta[j]=[0.]*L
-		for k in range(K):
-			for l in range(L):
-				npr[k][l]=[0.]*R
-		
-		
-	Like=0.
-	for n in linksr.keys():
-		ra=ratings.index(linksr[n])
-		D=0.
-		for l in range(L):
-			for k in range(K):
-				D=D+theta[n[0]][k]*eta[n[1]][l]*pr[k][l][ra]
-		for l in range(L):
-			for k in range(K):
-				Like=Like+(theta[n[0]][k]*eta[n[1]][l]*pr[k][l][ra])*log(D)/D
-	
-	print iterations,'iterations',Like,'Likelihood'
-	'''
-	fout=open('parameters_'+str(c)+'.dat',"w")
-	fout.write('%s\n' % Like)
-	for i in range(p):
-		for k in range(K):
-			fout.write('%s ' % theta[i][k])
-		fout.write('\n')
-	for j in range(m):
-		for l in range(L):
-			fout.write('%s ' % eta[j][l])
-		fout.write('\n')
-	for k in range(K):
-		for l in range(L):
-			for r in range(R):
-				fout.write('%s ' % pr[k][l][r])
-			fout.write('\n')
-	fout.close()
-	'''
-
-	#save the probability distribution for s of the links in rat
-	nl=0
-	for line in igot2:
-		about = line.strip().split('	')
-		for r in range(R):
-			pra=0.
-			for k in range(K):
-				for l in range(L):
-					pra=pra+theta[int(about[0])][k]*eta[int(about[1])][l]*pr[k][l][r]
-			rat[nl][r]=rat[nl][r]+pra/sampling
-		nl=nl+1
-	
-#Final evaluation of sampling:
-#The mode of the pdf of the ratings for each link is the best estimate for the accuracy and the median of the pdf is the best estimate for the MAE
-nl=0
-true=0
-s2=0.
-truepond=0
-s2pond=0.
-tot=0
-fout=open('predictions.dat','w')
-fout.write('user item real_rating prediction ratings_probability_distribution\n')
-for line in igot2:
-	about = line.strip().split('	')
-	ra=float(about[2])
-	best=rat[nl]
-	
-	rbest=best.index(max(best))
-	if sum(best)!=0.:
-		if ra==ratings[rbest]:
-			true=true+1
-		s2=s2+abs(ratings[rbest]-ra)
-		pond=0.
-		for r in range(R):
-			pond=pond+best[r]*ratings[r]
-		s2pond=s2pond+abs(pond-ra)
-		if ra==min(ratings, key=lambda x:abs(x-pond)):
-			truepond=truepond+1
-		tot=tot+1
-	
-	fout.write('%s %s %s %s ' % (about[0], about[1], ra, ratings[rbest]))
-	for r in range(R):
-		fout.write('%s ' % best[r])
-	fout.write('\n')
-	nl=nl+1
-print 'sampling', sampling
-print 'accuracy u most likely', true/float(tot), 'MAE u the median', truepond/float(tot)
-
+    for c in range(nsampling):
+        sampling(c, ofolder, linksr, nsampling, iterations, K, L, R,
+                 user_dict, n_users, users_denom, place_dict, n_places, places_denom, rat, verbose)
+        return "done!"
