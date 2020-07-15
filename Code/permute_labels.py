7e43fb2886e300ce28011d55eb20f1752ad91806
rogini98 <rogini.runghen@gmail.com>
2020-07-15 17:25:35 +1200

    updated names of files

--- Code/permute_labels.py
+++ Code/permute_labels.py
@@ -15,17 +15,17 @@ from collections import OrderedDict
 
 def permute_proba_mat(
 	reference = 1,
-    	folderin = "../Data/output/raw/prob-gp-gp/", 
-	folderout = "../Data/output/processed/reordered/proba-gp-gp/", 
-	rows = 10, 
-	columns = 10, 
+    	folderin = "../Data/output/raw/prob_mat/",
+	folderout = "../Data/output/processed/reordered/proba_mat/",
+	rows = 10,
+	columns = 10,
 	nsamples = 500):
-    
+
     # read in the best permutations from alignments
-    results = np.load('../Data/output/processed/alignments/1proba10gp-10gp.npy')
-    
+    results = np.load('../Data/output/processed/alignments/0proba10gp-10gp.npy')
+
     for iii in tqdm(range(0, nsamples)):
-        
+
         # if sampling no == reference, do not permute labels
         if iii==reference:
             continue
@@ -52,65 +52,65 @@ def permute_proba_mat(
 
 def permute_user_gp(
     reference = 41,
-	folderin = "../Data/output/raw/user-gp/", 
-	folderout = "../Data/output/processed/reordered/user-gp/", 
-	rows = 100, 
-	columns = 10, 
+	folderin = "../Data/output/raw/visitor_gp/",
+	folderout = "../Data/output/processed/reordered/visitor_gp/",
+	rows = 100,
+	columns = 10,
 	nsamples = 500):
-    
+
     # read in the best permutations from alignments
-    results = np.load('../Data/output/processed/alignments/1proba10gp-10gp.npy')
-    
+    results = np.load('../Data/output/processed/alignments/0proba10gp-10gp.npy')
+
     for iii in tqdm(range(0, nsamples)):
     	#print iii
         if iii==reference:
             continue
-        
+
         # read in the sample and reshape it in matrix format
-        mmat = [x.strip().split() for x in open(folderin+str(iii)+"_usergp10.dat", "r").readlines()]
+        mmat = [x.strip().split() for x in open(folderin+str(iii)+"_visitorgp10.dat", "r").readlines()]
 
         # get the user id
         user_id = np.asarray(mmat)[:,0]
         user_id = list(OrderedDict.fromkeys(user_id))
-        
-        ## select the 2nd column [contains proba of gp membership] 
+
+        ## select the 2nd column [contains proba of gp membership]
         mmat = np.asarray(mmat)[:,1].reshape((rows,columns)).astype(np.float)
         #print mmat.shape
         #print mmat[1,:]
 
         # extract permuted labels of sample from results
         permuted_labels = results[1:, iii]
-        
-        # now reorder sample 
+
+        # now reorder sample
         reordered_mat = mmat[:, permuted_labels[:columns]] # reorder columns of proba mat
-   
-	np.savetxt(folderout+str(iii)+"reordered_usergp10.txt", reordered_mat)
+
+	np.savetxt(folderout+str(iii)+"reordered_visitorgp10.txt", reordered_mat)
 
 def permute_place_gp(
     reference = 41,
-	folderin = "../Data/output/raw/place-gp/", 
-	folderout = "../Data/output/processed/reordered/place-gp/", 
-	rows = 100, 
-	columns = 10, 
+	folderin = "../Data/output/raw/place-gp/",
+	folderout = "../Data/output/processed/reordered/place_gp/",
+	rows = 100,
+	columns = 10,
 	nsamples = 500):
-    
+
     # read in the best permutations from alignments
-    results = np.load('../Data/output/processed/alignments/1proba10gp-10gp.npy')
+    results = np.load('../Data/output/processed/alignments/0proba10gp-10gp.npy')
 
     for iii in tqdm(range(0, nsamples)):
-        
+
         if iii==reference:
             continue
 
         # read in the sample and reshape it in matrix format
-        mmat = [x.strip().split() for x in open(folderin+str(iii)+"_placegp8.dat", "r").readlines()]
-        
+        mmat = [x.strip().split() for x in open(folderin+str(iii)+"_placegp10.dat", "r").readlines()]
+
         # get the place id
         place_id = np.asarray(mmat)[:,0]
         place_id = list(OrderedDict.fromkeys(place_id))
         print len(place_id) # yay ca marche poulette!
-        print place_id 
-        
+        print place_id
+
 
         ## select the 1st column and format it
         mmat = np.asarray(mmat)[:,1].reshape((rows,columns)).astype(np.float)
@@ -120,7 +120,7 @@ def permute_place_gp(
         permuted_labels = results[1:, iii]
         print permuted_labels
         print permuted_labels[-columns:] # permuted labels of columns
-     
-        # now reorder sample 
+
+        # now reorder sample
         reordered_mat = mmat[:, permuted_labels[-columns:]] # reorder columns of proba mat
-        np.savetxt(folderout+str(iii)+"reordered_placegp8.txt", reordered_mat)
+        np.savetxt(folderout+str(iii)+"reordered_placegp10.txt", reordered_mat)
